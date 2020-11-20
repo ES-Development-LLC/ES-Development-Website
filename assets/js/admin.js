50eb5e3dfc0916c4bf22c8cad8aaf700a030a176
Emmett S <30877743+RealEmmettS@users.noreply.github.com>
2020-11-19 23:38:01 -0600

    login_bugFix

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,10 +1,37 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    window.alert("Signed in successfully!")
+  } else {
+    // No user is signed in.
+  }
+});
+//////////////////////////////////////////////////////
+
 function login(){
+  var success = true;
   try{
-    var userEmail = document.getElementByID("email_field").value;
-    var userPass = document.getElementByID("password_field").value;
-    window.alert(userEmail + " " + userPass);
-  } catch{
-    window.alert("Error getting Username and Password");
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+    //window.alert(userEmail + " " + userPass);
+
+  } catch(err) {
+    success = false;
+    window.alert("Error:" + err.message);
+
+  } if (success){
+
+      firebase.auth().signInWithEmailAndPassword(userEmail, userPass)
+    .then((user) => {
+      // Signed in
+      window.alert("Signed in as "+ userEmail);
+    })
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      window.alert("Error: " + errorMessage);
+    });
+
   }
 
 
